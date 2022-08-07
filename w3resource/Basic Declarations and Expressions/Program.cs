83454632686163dbac271a1455bb3a6185d65601
reazuahmed <reaz.bv@gmail.com>
2022-08-07 14:42:40 -0400

    Add files via upload
    Changes saved.

--- w3resource/Basic Declarations and Expressions/Program.cs
+++ w3resource/Basic Declarations and Expressions/Program.cs
@@ -11,8 +11,8 @@ namespace Basic_Declarations_and_Expressions
         static void Main(string[] args)
         {
             //Exercises Methods:
-            Help.NumberPatterns.Pyramid();
-            
+            //Help.NumberPatterns.Pyramid();
+
             //Exercises.PrintHelloAndName();
             //Exercises.PrintSumOf2Numbers();
             //Exercises.Divide2Numbers();
@@ -26,6 +26,47 @@ namespace Basic_Declarations_and_Expressions
             //Exercises.TakeAgeAndPrintSomething();
             //Exercises.DisplayTakenNumber4TimesInSingleRow();
             //Exercises.CreateRectangleUsingInputNumber();
+            //Exercises.ConvertFromCelciusToKelvinAndFarenheit();
+            //Exercises.RemoveSpecifiedCharacterFromNonEmptyStringUsingIndexOfACharacter();
+            //Exercises.CreateNewStringFromGivenStringWhereFirstAndLastCharChangePosition();
+            //Exercises.CreateNewStringFromGivenStringWithFirstCharAddedAtFrontAndBack();
+            //Exercises.Check2GivenIntersAndReturnTruIfOneIsNegativeAndOneIsPositive();
+            //Exercises.ComputeSumOf2GivenIntegersIf2ValuesAreEqualReturnTripleOfTheirSum();
+            //Exercises.GetAbsoluteValueOfDiffBetween2GivenNumbersReturnDoubleTheAbsoluteValueOfDiffIfFistNumGreaterThanSecondNum();
+            //Exercises.GetAbsoluteValueOfDiffBetween2GivenNumbersReturnDoubleTheAbsoluteValueOfDiffIfFistNumGreaterThanSecondNum_1();
+            //Exercises.CheckSumOfTwoGivenIntegersAndReturnTrueIfOneIntegerIs20OrTheirSumIs20();
+            //Exercises.CheckIfGivenIntegerIsWithin20Of100Or200();
+            //Exercises.ConvertGivenStringIntoLowerCase();
+            //Exercises.FindLongestWordInString();
+            //Exercises.PrintOddNumbersFrom1to99();
+            //Exercises.ComputeSumOfFirst500PrimeNumbers();
+            //Exercises.ComputeSumOfDigitsOfAnInteger();
+            //Exercises.ReverseTheWordsOfASentence();
+            //Exercises.FindSizeOfFileInBytes();
+            //Exercises.ConvertHexaDecimalNumberToDecimalNumber();
+            //Exercises.MultiplyCorrespondingElementsOfTwoArraysOfIntegers();
+            //Exercises.CreateNewStringOf4CopiesWithLast4CharactersFromAGivenString();
+            //Exercises.CheckIfTheGivenPositiveNumberIsMultipleOr3Or7();
+            //Exercises.MatchStartingString();
+            //Exercises.MatchStartingString1();
+            //Exercises.Check2NumbersIfLessOrGreaterThanRespectively100And200();
+            //Exercises.CheckIfTheGivenIntegerIsInTheRangeFromMinus10To10();
+            //Exercises.CheckIfHPAppearsAt2ndPositionInAStringAndReturnsTheStringWithoutHP();
+            //Exercises.GetNewStringOf2CharactersFromAGivenString();
+            //Exercises.FindLargestAndLowestValuesfrom3IntegerValues();
+            //Exercises.CheckNearestValueOf20Of2GivenIntegersAndReturns0If2NumbersAreSame();
+            //Exercises.CheckIfGivenStringContainsACharacterWBetween1And3Times();
+            //Exercises.CreateNewStringWhereFirst4CharactersWillBeInLowerCase();
+            //Exercises.CheckIfGivenStringStartsWithW();
+            //Exercises.CreateNewStringOfEveryOtherCharacterFromTheFirstPositionOfGivenString();
+            //Exercises.CountSpecifiedNumberInGivenArrayOfInteger();
+            //Exercises.CheckIfTheNumberAppearsAsFirstOrLastElementOfTheArrayOfIntegersAndTheLengthIs1OrMore();
+            //Exercises.CheckIfTheNumberAppearsAsFirstOrLastElementOfTheArrayOfIntegersAndTheLengthIs1OrMore1();
+            //Exercises.ComputeTheSumOfAllTheElementsOfAnArrayOfIntegers();
+            //Exercises.CheckIfFirstElementAndLastElementAreEqualOfAnArrayOfIntegersAndTheLengthIs1OrMore();
+            //Exercises.CheckIfFirstOrLastElementOfTwoArraysAreEqual();
+            //Exercises.RotateArrayOfIntegersInLeftDirection();
+            Exercises.GetLargerValueBetweenFirstAndLastElementOfAnArrayOfIntegers();
         }
     }
 }
